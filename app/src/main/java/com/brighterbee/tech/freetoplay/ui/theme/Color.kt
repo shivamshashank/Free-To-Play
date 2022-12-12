8f10291d62bee6ac32b5419fd00496ce8c233661
Shivam Shashank <shivamkumar872000@gmail.com>
2022-12-12 22:43:56 +0530

    Starter Project.

--- app/src/main/java/com/brighterbee/tech/freetoplay/ui/theme/Color.kt
+++ app/src/main/java/com/brighterbee/tech/freetoplay/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.brighterbee.tech.freetoplay.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Gray300 = Color(0xFFe0e0e0)
+val Gray400 = Color(0xFFbdbdbd)
+
+val Black1 = Color(0xFF222222)
+
+val White = Color(0xFFFFFFFF)
+
+val RedErrorDark = Color(0xFFB00020)
+val RedErrorLight = Color(0xFFEF5350)
--- app/src/main/java/com/brighterbee/tech/freetoplay/ui/theme/Theme.kt
+++ app/src/main/java/com/brighterbee/tech/freetoplay/ui/theme/Theme.kt
@@ -1,44 +1,60 @@
 package com.brighterbee.tech.freetoplay.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightThemeColors = lightColors(
+    primary = Color(0xFF546ee5),
+    primaryVariant = Color(0xFF8c9cff),
+    onPrimary = White,
+    secondary = Color(0xFF46807E),
+    secondaryVariant = Color(0xFF75b0ad),
+    onSecondary = Black1,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Color(0xFF070d2d),
+    onBackground = White,
+    surface = Color(0xFF161A37),
+    onSurface = Gray300,
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val DarkThemeColors = darkColors(
+    primary = Color(0xFF546ee5),
+    primaryVariant = Color(0xFF8c9cff),
+    onPrimary = White,
+    secondary = Color(0xFF46807E),
+    secondaryVariant = Color(0xFF75b0ad),
+    error = RedErrorLight,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = Black1,
+    onSurface = Color.White,
 )
 
 @Composable
-fun FreeToPlayTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun FreeToPlayTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     MaterialTheme(
-        colors = colors,
-        typography = Typography,
+        colors = if (darkTheme) DarkThemeColors else LightThemeColors,
+        typography = KaiseioptiTypography,
         shapes = Shapes,
-        content = content
-    )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.surface)
+        ) {
+            content()
+        }
+    }
 }
